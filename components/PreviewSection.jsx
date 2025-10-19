5b8fb6c9a46aa6545eda930c7f3bdd83339d9151
milles1234 <dy02082003@gmail.com>
2025-10-19 04:25:10 +0000

    StudiioLighting of macbook

--- components/PreviewSection.jsx
+++ components/PreviewSection.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import clsx from "clsx";
 import useMackbookStore from '../src/store'
+import { Canvas } from '@react-three/fiber'
+import { Box, OrbitControls } from '@react-three/drei';
+import MacbookModel14 from './models/Macbook-14';
 
+import StudioLights from './StudioLights';
 
 
 
@@ -11,7 +15,7 @@ const PreviewSection = () => {
     <section id='product-viewer'>
       <h2 >Take a closer look.</h2>
       <div className="controls">
-            <p className='info'>Macbook Pro in space black</p>
+            <p className='info'>Macbook Pro {scale} in {color}</p>
 
             <div className='flex-center mt-5 gap-5'>
               <div className='color-control'>
@@ -28,14 +32,25 @@ const PreviewSection = () => {
                        
                 </div>
                  <div className="size-control">
-                  <div><p>14"</p></div>
-                  <div><p>16"</p></div>
+                  <div
+                   onClick={() => setScale(0.06)}
+                   className={clsx(scale === 0.06 ? 'bg-white text-black': 'bg-transparent text-white'  )}
+                  ><p>14"</p></div>
+                  <div
+                   onClick={() => setScale(0.08)}
+                   className={clsx(scale === 0.08 ? 'bg-white text-black': 'bg-transparent text-white'  )}
+                  ><p>16"</p></div>
 
               </div>
             </div>
       </div>
             
         <p className='text-white text-4xl'>Render Canvas</p>
+         <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100}}>
+                <StudioLights/>
+                <MacbookModel14 scale= {0.06} position={[0,0,0]}/>
+                <OrbitControls enableZoom={false} />
+            </Canvas>
     </section>
   )
 }
